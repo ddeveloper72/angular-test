def1a6ae5cb19d7f1fe431657f7425d6c69af17a
ddeveloper72 <duncan.niamh@gmail.com>
2020-05-28 20:48:49 +0100

    demonstration of OnInit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-intro';
   name = 'Duncan';
   today = new Date();
   counter = 0;
   people = [
-    {name: 'Duncan', age: 24},
-    {name: 'You', age: 35},
-    {name: 'Sam', age: 28}
+    { name: 'Duncan', age: 24 },
+    { name: 'You', age: 35 },
+    { name: 'Sam', age: 28 }
   ];
+
+  ngOnInit() {
+    console.log('component initiated');
+  }
 
   btnClicked = () => {
     this.counter++;
-  }
-
+  };
 }
