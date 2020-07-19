596b3313383d9ef596629216c31bf58943a6ab20
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-19 10:56:45 +0100

    added a new app module called child component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { PeopleService } from './people.service';
 import { AppRoutingModule } from './app-routing.module';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FirstComponent,
-    SecondComponent
+    SecondComponent,
+    ChildComponent
   ],
   imports: [
     BrowserModule,
