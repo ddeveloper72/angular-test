b5c744d3d8d1280f0ca60b22dd8894ac5ffe6b32
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-22 22:38:30 +0100

    Add event emitter with avtiable name called changed

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -8,8 +8,11 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ChildComponent implements OnInit {
 
   @Input() name: string;
+  @Output() changed = new EventEmitter();
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.changed.emit('Not Bob');
+  }
 }
