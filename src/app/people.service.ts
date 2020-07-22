3ce3473140aaa336f400cbdaa56c673300aa90b6
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-22 23:05:11 +0100

    setup a new observable on PeopleService

--- src/app/people.service.ts
+++ src/app/people.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeopleService {
+
+newObservable$ = new Observable( observer => {
+  observer.next();
+});
+
   people = [
     { name: 'Duncan', age: 24 },
     { name: 'You', age: 35 },
