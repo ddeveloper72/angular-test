77c14c212f4792f45e7dac0ea429ab240bbad3e9
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-22 22:26:53 +0100

    Define the Input from the parent component imported into the child componend

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,9 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
 
-  constructor() { }
+  @Input() name: string;
 
-  ngOnInit(): void {
-  }
+  constructor() {}
 
+  ngOnInit(): void {}
 }
